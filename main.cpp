7ef6eaaa18fa66d354ef7e9a21bb3078bf725e0e
Ze81124ro <157675123+Ze81124ro@users.noreply.github.com>
2024-01-26 20:02:36 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,9 +2,9 @@
 #include "vector.hpp"
 
 int main() {
-    Vector<int> v = {1, 2, 3, 4, 5};
-    v.insert(v.begin() + 3, 6);
-    for (int i : v)
-        std::cout << i << "\n";
+    // Vector<int> v = {1, 2, 3, 4, 5};
+    // v.insert(v.begin() + 3, 6);
+    // for (int i : v)
+    //     std::cout << i << "\n";
     std::cout << "Successfuly!!!\n";
 }
