80ff9c75f2efdba1c2b5d7cee4ba18a0ae277619
Ze81124ro <157675123+Ze81124ro@users.noreply.github.com>
2024-01-27 13:35:56 +0400

    Update iterator.cpp

--- iterator.cpp
+++ iterator.cpp
@@ -6,7 +6,7 @@ constexpr Vector<T>::directed_iterator<dir, mut>::directed_iterator() noexcept: 
 
 template <class T>
 template <Direction dir, Mutability mut>
-constexpr Vector<T>::directed_iterator<dir, mut>::directed_iterator(typename Vector<T>::directed_iterator<dir, mut>::pointer data) noexcept: data(data) {}
+constexpr Vector<T>::directed_iterator<dir, mut>::directed_iterator(pointer data) noexcept: data(data) {}
 
 template <class T>
 template <Direction dir, Mutability mut>
@@ -16,15 +16,66 @@ Vector<T>::directed_iterator<dir, mut>::operator directed_iterator<dir, Mutabili
 
 template <class T>
 template <Direction dir, Mutability mut>
-typename Vector<T>::directed_iterator<dir, mut>::reference Vector<T>::directed_iterator<dir, mut>::operator*() const noexcept {
+auto Vector<T>::directed_iterator<dir, mut>::operator*() const noexcept -> reference {
     return *data;
 }
 
 template <class T>
 template <Direction dir, Mutability mut>
-typename Vector<T>::directed_iterator<dir, mut>::pointer Vector<T>::directed_iterator<dir, mut>::operator->() const noexcept {
+auto Vector<T>::directed_iterator<dir, mut>::operator->() const noexcept -> pointer {
     return data;
 }
 
-// template <class T>
-// template <>
+auto Vector<T>::directed_iterator<dir, mut>::operator[](difference_type n) const noexcept -> reference {
+    return *(*this + n);
+}
+
+template <class T>
+template <Direction dir, Mutability mut>
+auto Vector<T>::directed_iterator<dir, mut>::operator+=(difference_type n) & noexcept -> directed_iterator& {
+    data += n * static_cast<int>(dir);
+    return *this;
+}
+
+template <class T>
+template <Direction dir, Mutability mut>
+auto Vector<T>::directed_iterator<dir, mut>::operator-=(difference_type n) & noexcept -> directed_iterator& {
+    return *this += -n;
+}
+
+template <class T>
+template <Direction dir, MUtability mut>
+auto Vector<T>::directed_iterator<dir, mut>::operator++() & noexcept -> directed_iterator& {
+    return *this += 1;
+}
+
+auto Vector<T>::directed_iterator<dir, mut>::operator--() & noexcept -> directed_iterator& {
+    return *this += -1;
+}
+
+auto Vector<T>::directed_iterator<dir, mut>::operator++(int) & noexcept -> directed_iterator {
+    directed_iterator res = *this;
+    ++(*this);
+    return res;
+}
+
+auto Vector<T>::directed_iterator<dir, mut>::operator--(int) & noexcept -> directed_iterator {
+    directed_iterator res = *this;
+    --(*this);
+    return res;
+}
+
+auto Vector<T>::directed_iterator<dir, mut>::operator+(const directed_iterator& it, difference_type n) -> directed_iterator {
+    directed_iterator res = it;
+    res += n;
+    return res;
+}
+
+auto Vector<T>::directed_iterator<dir, mut>::operator+(difference_type n, const directed_iterator& it) -> directed_iterator {
+    return it + n;
+}
+
+auto Vector<T>::directed_iterator<dir, mut>::operator-(const directed_iterator& it1, const directed_iterator& it2) -> difference_type {
+    return it1.data - it2.data;
+}
+
